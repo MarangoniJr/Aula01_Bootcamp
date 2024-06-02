3d2a6166a506d8e866a6f55a72885d15b6fdd832
Paulo Marangoni Junior <paulocmarangoni@hotmail.com>
2024-06-02 18:51:50 -0300

    segundo commit de aprendizado

---
+++ app/exemplo_01.py
@@ -0,0 +1,7 @@
+nome  = input("Digite o seu nome: ")
+tamanhoNome = len(nome)
+
+if tamanhoNome < 5:
+    print('Nome de usuário com quantidade de caracteres não permitido!')
+else:
+    print('Olá ' + nome + ' espero que esteja bem ! Seja bem vindo !') 
